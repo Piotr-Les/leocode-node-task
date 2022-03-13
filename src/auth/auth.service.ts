dfe1318b83f2823a0f20f1ea5be8db9eec8f1ab2
Piotr Leś <->
2022-03-13 17:04:01 +0100

    add user and auth modules with services

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { UserData } from 'src/types';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async checkUsersCredentials(email: string, password: string) {
+    const user = await this.usersService.findOne(email);
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  login(user: UserData) {
+    const payload = { email: user.email, sub: user.id };
+
+    return {
+      authToken: this.jwtService.sign(payload),
+    };
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+export type User = {
+  id: number;
+  email: string;
+  password: string;
+};
+
+@Injectable()
+export class UsersService {
+  private users: User[];
+
+  constructor() {
+    this.users = [
+      {
+        id: 1,
+        email: 'user1@example.com',
+        password:
+          '$2a$04$FRTr5yCAXNCZ3f2w7q1qBej8Jdcapv/eCZU.nAMmamClitSsxM2vG',
+      },
+      {
+        id: 2,
+        email: 'user2@example.com',
+        password:
+          '$2a$04$9ZNCaGq.bejyHbebdsFaN.x4rNMvdpOOrSVnVhVXHnUPAEB/RNK6u',
+      },
+      {
+        id: 3,
+        email: 'user3@example.com',
+        password:
+          '$2a$04$AAJPqIRULvIg7x8SV2xFz.U84aaPNDl9C5NlrnRbH5IzTiBS2ioTC',
+      },
+    ];
+  }
+
+  async findOne(email: string): Promise<User | undefined> {
+    return this.users.find((user) => user.email === email);
+  }
+}
